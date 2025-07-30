6a7d2d1d9a2f59ba69fc9ed1bfd82e7d9458b40c
ariq <23016438@myrp.edu.sg>
2025-07-30 16:48:05 +0800

    automate add case

---
+++ frontend/src/pages/userData.js
@@ -0,0 +1,23 @@
+// ✅ Wrap it in an array
+export const userData = [
+  {
+    driverId: 'GEODR216600',
+    driverImage: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Chong Theng Ng',
+    type: 'Ride-Hailing',
+    contact: '90527878',
+    email: 'chongthengng@gmail.com',
+    fleetType: 'Taxi (TDVL)',
+    userType: 'Driver'
+  },
+  {
+    driverId: 'GEODR543210',
+    driverImage: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=2000&fit=crop',
+    name: 'Faye Lim',
+    type: 'Delivery',
+    contact: '91234567',
+    email: 'faye.lim@example.com',
+    fleetType: 'Car (PDVL)',
+    userType: 'Driver'
+  }
+];
